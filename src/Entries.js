4bc8db9cddd92ad2ba71a905d79495262cdc1ba7
jvog <62656589+jensvog@users.noreply.github.com>
2020-05-17 09:54:52 +0200

    Add functionality for adding elements
    * Added post message function. New message will be displayed on top
    * Added form for adding new message
    * Edited button style

--- src/Entries.js
+++ src/Entries.js
@@ -4,7 +4,19 @@ import { Auth } from 'aws-amplify';
 
 const axios = require('axios').default;
 
+async function getHeaderConfig() {
+  const user = await Auth.currentSession();
+  const token = user.getIdToken().getJwtToken();
+  return {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+}
+
 class Entries extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createEntry = this.createEntry.bind(this)
+  }
   componentDidMount() {
     this.update()
   }
@@ -29,6 +41,21 @@ class Entries extends React.Component {
         })
     });
   }
+  async createEntry() {
+    let { channelId } = this.props.match.params
+    const config = await getHeaderConfig();
+    console.log(config)
+    const request = {
+      channelId,
+      message: this.refs.post.value
+    }
+    const response = await axios.post('https://qnjkiuaakb.execute-api.eu-central-1.amazonaws.com/dev/createEntry', request, config)
+
+    console.log(response.data)
+    this.setState((state, props) => ({
+        entries: new Array().concat(response.data, state.entries)
+    }))
+  }
   state = {
     entries: []
   }
@@ -36,6 +63,15 @@ class Entries extends React.Component {
   render() {
     return (
       <>
+        <div>
+          <div class="heading">
+            <em>Write a post</em>
+          </div>
+          <div class="list">
+            <textarea ref="post">Write a post</textarea>
+            <button onClick={this.createEntry}>Post</button>
+          </div>
+        </div>
         {this.state.entries.map((entry) => (
           <div>
             <div class="heading">

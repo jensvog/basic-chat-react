9ff4dc538e953313d7a48f8186a3942674da80e5
jvog <62656589+jensvog@users.noreply.github.com>
2020-05-21 11:20:59 +0200

    Clean up App.js

--- src/App.js
+++ src/App.js
@@ -10,28 +10,26 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-      <table>
-        <tbody>
-          <tr>
-            <td colSpan="2">
-              <Heading />
-            </td>
-          </tr>
-          <tr>
-            <td class="channels" id="channels">
-              <Channels />
-            </td>
-            <td>
-              <Switch>
-                <Route path="/channel/:channelId" component={Entries} />
-                <Route path="/">
-                  <Start />
-                </Route>
-              </Switch>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+        <table>
+          <tbody>
+            <tr>
+              <td colSpan="2">
+                <Heading />
+              </td>
+            </tr>
+            <tr>
+              <td class="channels" id="channels">
+                <Channels />
+              </td>
+              <td>
+                <Switch>
+                  <Route path="/channel/:channelId" component={Entries} />
+                  <Route path="/" component={Start} />
+                </Switch>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </Router>
     )
   }

ecadb109ab8ad7e4f8bd26da9249c8e39531e873
jvog <62656589+jensvog@users.noreply.github.com>
2020-05-23 12:55:11 +0200

    Remove class warning from Start.js

--- src/Start.js
+++ src/Start.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Start = () => {
     return (
         <div>
-        <div class="heading">Please choose a channel</div>
+        <div className="heading">Please choose a channel</div>
         </div>
     )
 }
